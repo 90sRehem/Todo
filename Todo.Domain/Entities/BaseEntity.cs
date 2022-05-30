21df7e6841e5db4ee82472931641904576a9d2c7
90sRehem <you@example.com>
2022-05-30 18:40:09 -0300

    first commit

---
+++ Todo.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+namespace Todo.Domain.Entities
+{
+    public abstract class BaseEntity : IEquatable<BaseEntity>
+    {
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+
+        public bool Equals(BaseEntity? other)
+        {
+            return Id == other.Id;
+        }
+    }
+}
