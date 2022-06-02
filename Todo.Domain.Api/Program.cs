98fc9b87a3d8e8d5b575ddda56c601315f7493c3
90sRehem <you@example.com>
2022-06-02 17:04:41 -0300

    feat

--- Todo.Domain.Api/Program.cs
+++ Todo.Domain.Api/Program.cs
@@ -1,25 +1,58 @@
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+using Todo.Domain.Handlers;
+using Todo.Domain.Infra.Contexts;
+using Todo.Domain.Infra.Repositories;
+using Todo.Domain.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigureServices(builder);
+ConfigureMvc(builder);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.UseRouting();
+app.UseCors(x =>
+    x.AllowAnyOrigin()
+    .AllowAnyHeader()
+    .AllowAnyMethod());
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
 app.Run();
+
+void ConfigureMvc(WebApplicationBuilder builder)
+{
+    builder
+      .Services
+      .AddControllers()
+      .ConfigureApiBehaviorOptions(options => { options.SuppressModelStateInvalidFilter = true; })
+      .AddJsonOptions(x =>
+      {
+          x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+          x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+      });
+}
+
+void ConfigureServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddTransient<ITodoRepository, TodoRepository>();
+    builder.Services.AddTransient<TodoHandler, TodoHandler>();
+    builder.Services.AddDbContext<DataContext>(
+        options =>
+            options.UseInMemoryDatabase("Database"));
+
+    // builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlServer(ConfigurationBinder.GetConnectionString("ConnectionString")));
+}
